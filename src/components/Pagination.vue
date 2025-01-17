59c6d59a7b008b452c6f7d867b9c9d2af267bad7
AndoMahefa <razafimahatratramahefa7@gmail.com>
2025-01-17 19:47:28 +0300

    Creation du composant pagination

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,66 +1,40 @@
 <template>
-  <div class="d-flex justify-center mt-4">
-    <v-btn
-      :disabled="page === 1"
-      class="mx-1"
-      @click="changePage(page - 1)"
-    >
-      Précédent
-    </v-btn>
-
-    <v-btn
-      v-for="p in pageNumbers"
-      :key="p"
-      :class="{'v-btn--active': page === p}"
-      class="mx-1"
-      @click="changePage(p)"
-    >
-      {{ p }}
-    </v-btn>
-
-    <v-btn
-      :disabled="page === totalPages"
-      class="mx-1"
-      @click="changePage(page + 1)"
-    >
-      Suivant
-    </v-btn>
-  </div>
+  <v-row justify="center">
+    <v-col cols="8">
+      <v-container class="max-width">
+        <v-pagination
+          :model-value="currentPage"
+          :length="totalPages"
+          class="my-4"
+          rounded="circle"
+          @update:model-value="onPageChange" 
+        />
+      </v-container>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-    export default {
-        props: {
-            page: {
-                type: Number,
-                required: true,
-            },
-            totalPages: {
-                type: Number,
-                required: true,
-            },
-        },
-        emits: ['update:page'],
-        computed: {
-            pageNumbers() {
-                const range = [];
-                for (let i = 1; i <= this.totalPages; i++) {
-                range.push(i);
-                }
-                return range;
-            },
-        },
-        methods: {
-            changePage(page) {
-                this.$emit('update:page', page);
-            },
-        },
-    };
-    </script>
+export default {
+  name: 'Pagination',
+  props: {
+    totalPages: {
+      type: Number,
+      required: true,
+    },
+    currentPage: {
+      type: Number,
+      required: true,
+    },
+  },
+  methods: {
+    onPageChange(page) {
+      // Émettez un événement pour informer le parent
+      this.$emit('page-changed', page);
+    },
+  },
+};
+</script>
 
 <style scoped>
-    .v-btn--active {
-        background-color: #72dae8;
-        color: white;
-    }
 </style>
